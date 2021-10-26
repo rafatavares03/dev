fc82958bd25ce409bb745ff1af205308fd1f1f65
Rafael Tavares <rafatavares03@hotmail.com>
2021-10-25 22:23:52 -0300

    improvement sass code part 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 document.querySelector('#menuButton').addEventListener('click', () => {
-    const mobileMenu = document.querySelector('#mobileMenu');
-    if (mobileMenu.classList.contains('open')) {
-        mobileMenu.classList.remove('open');
-    } else {
-        mobileMenu.classList.add('open');
-    }
+  const mobileMenu = document.querySelector('#mobileMenu');
+  if (mobileMenu.classList.contains('open')) {
+    mobileMenu.classList.remove('open');
+  } else {
+    mobileMenu.classList.add('open');
+  }
 });
 
 document.querySelector('#mobileMenu').addEventListener('click', (e) => {
-    if (e.target.classList.contains('open')) {
-        document.querySelector('.mobile-menu.open').classList.remove('open');
-    }
+  if (e.target.classList.contains('open')) {
+    document.querySelector('.mobile-menu.open').classList.remove('open');
+  }
 })
